c8b5fdc46de36ac58d9b8be2022e61ddf9e424de
karthik261201 <karthiknarayanan043@gmail.com>
2024-09-24 07:37:17 +0530

    lecture-1

---
+++ App.js
@@ -0,0 +1,17 @@
+// Basic way of creating HTML content
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World!! from React")
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child-1" }, [
+        React.createElement("h1", {}, "C1 H1 Tag"),
+        React.createElement("h2", {}, "C1 H2 Tag")
+    ]),
+    React.createElement("div", { id: "child-2" }, [
+        React.createElement("h1", {}, "C2 H1 Tag"),
+        React.createElement("h2", {}, "C2 H2 Tag")
+    ]),
+])
+
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
