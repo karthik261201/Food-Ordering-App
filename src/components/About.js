df6e52fa57c31b2ef4a1ffbcf81ceb03d7058862
karthik261201 <karthiknarayanan043@gmail.com>
2024-09-27 10:36:33 +0530

    after lec-8

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
         <div>
             <h1>About</h1>
             <h2>React Course</h2>
+            <UserClass name="Karthik" location="Kottayam" />
         </div>
     );
 }
---
+++ src/components/UserClass.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class UserClass extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            userInfo: {
+                name: "Dummy",
+                location: "Dummy"
+            }
+        }
+    }
+
+    async componentDidMount() {
+        const data = await fetch("https://api.github.com/users/akshaymarch7")
+        const json = await data.json()
+        console.log(json)
+
+        this.setState({
+            userInfo: json
+        })
+    }
+
+    componentDidUpdate() {
+        console.log("Component did Update")
+    }
+
+    componentWillUnmount() {
+        console.log("Component will unmount")
+    }
+
+    render() {
+        const { name, location } = this.state.userInfo
+
+        return (
+            <div className="user-card">
+                <h2>Name : {name}</h2>
+                <h3>Location : {location}</h3>
+                <h4>Contact : @karthik07</h4>
+            </div>
+        );
+    }
+}
+
+export default UserClass;
